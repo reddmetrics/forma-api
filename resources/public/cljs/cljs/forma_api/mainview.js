a3f6b863500d7ea4d7b01d584f9a6b09cfffbd68
Aaron Steele <eightysteele@gmail.com>
2013-03-26 19:37:09 -0700

    reeeeeboot

--- resources/public/cljs/cljs/forma_api/mainview.js
+++
@@ -1,7 +0,0 @@
-goog.provide('forma_api.mainview');
-goog.require('cljs.core');
-goog.require('goog.events');
-forma_api.mainview.page_load = (function page_load(){
-return "Do some initial stuff.";
-});
-goog.events.listen.call(null,window,"load",forma_api.mainview.page_load);
--- src/cljs/forma/api/mainview.cljs
+++
@@ -1,7 +0,0 @@
-(ns forma.api.mainview
-  (:require [goog.events :as events]))
-
-(defn page-load []
-  "Do some initial stuff.")
-
-(events/listen js/window "load" page-load)
